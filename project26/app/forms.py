a03668c72a1da0daf3a3e3ff776e181a5a859e55
Saikiran <marveloftheuniverse@gmail.com>
2025-02-05 12:54:34 +0530

    Add files via upload

---
+++ project26/app/forms.py
@@ -0,0 +1,79 @@
+from django import forms
+from app.models import *
+
+def check_for_k(value):
+    if value.lower()[0]=='k':
+        raise forms.ValidationError('Name is Started with k')
+
+def check_for_len(value):
+    if len(value)<=5:
+        raise forms.ValidationError('len is < 5')
+
+class TopicForm(forms.Form):
+    topic_name=forms.CharField(validators=[check_for_k,check_for_len])
+
+
+
+class TopicModelForm(forms.ModelForm):
+    class Meta:
+        model=Topic
+        fields='__all__'
+
+class WebpageForm(forms.Form):
+    topic_name=forms.ModelChoiceField(queryset=Topic.objects.all())
+    name=forms.CharField()
+    url=forms.URLField()
+    email=forms.EmailField()
+    reemail=forms.EmailField()
+    botcatcher=forms.CharField(widget=forms.HiddenInput,required=False)
+
+    def clean_botcatcher(self):
+        bot=self.cleaned_data['botcatcher']
+        if len(bot)>0:
+            raise forms.ValidationError('bot has catched')
+
+    def clean(self):
+        e=self.cleaned_data['email']
+        re=self.cleaned_data['reemail']
+        if e!=re:
+            raise forms.ValidationError('emails are not matched')
+
+
+
+
+
+
+
+
+class WebpageModelForm(forms.ModelForm):
+    reemail=forms.EmailField()
+    bot=forms.CharField(widget=forms.HiddenInput,required=False)
+    class Meta:
+        model=Webpage
+        fields='__all__'
+    
+    def clean_bot(self):
+        bott=self.cleaned_data['bot']
+        if len(bott)>0:
+            raise forms.ValidationError('bot has catched')
+
+    def clean(self):
+        e=self.cleaned_data['email']
+        re=self.cleaned_data['reemail']
+        if e!=re:
+            raise forms.ValidationError('emails are not matched')
+
+
+        
+        
+        
+        
+        
+        
+        #fields=['topic_name','name','url']
+        #exclude=['email']
+        #labels={'name':'WebpageName'}
+        #widgets={'topic_name':forms.RadioSelect}
+        #help_texts={'topic_name':'THis Is Belongs To PT'}
+
+
---
+++ project26/app/views.py
@@ -0,0 +1,98 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+# Create your views here.
+from app.forms import *
+from app.models import *
+from django.http import HttpResponse
+
+def insert_topic(request):
+    ETFO=TopicForm()
+    d={'ETFO':ETFO}
+
+    if request.method=='POST':
+        TFDO=TopicForm(request.POST)
+        if TFDO.is_valid():
+            topic_name=TFDO.cleaned_data['topic_name']
+            LTO=Topic.objects.get_or_create(topic_name=topic_name)
+            if LTO[1]:
+                return HttpResponse('New Topic is Created')
+            else:
+                return HttpResponse('Topic Is Already present')
+        else:
+            return HttpResponse('Invalid data')
+    return render(request,'insert_topic.html',d)
+
+
+
+def insert_topic_by_mf(request):
+    ETMFO=TopicModelForm()
+    d={'ETMFO':ETMFO}
+    if request.method=='POST':
+        TMFDO=TopicModelForm(request.POST)
+        if TMFDO.is_valid():
+            TMFDO.save()
+            return HttpResponse('Topic is Created')
+        else:
+            return HttpResponse('Invalid Data')
+
+
+    return render(request,'insert_topic_by_mf.html',d)
+
+
+
+
+def insert_webpage(request):
+    EWFO=WebpageForm()
+    d={'EWFO':EWFO}
+
+    if request.method=='POST':
+        WDFO=WebpageForm(request.POST)
+        if WDFO.is_valid():
+            topic_name=WDFO.cleaned_data['topic_name']
+            name=WDFO.cleaned_data['name']
+            print(name)
+            url=WDFO.cleaned_data['url']
+            email=WDFO.cleaned_data['email']
+            TO=Topic.objects.get(topic_name=topic_name)
+            WO=Webpage.objects.get_or_create(topic_name=TO,name=name,email=email,url=url)
+
+            if WO[1]:
+                return HttpResponse('Webpage is created')
+        else:
+            return HttpResponse('InValid Data')
+            
+    return render(request,'insert_webpage.html',d)
+
+def insert_webpage_by_mf(request):
+    EWMFO=WebpageModelForm()
+    d={'EWMFO':EWMFO}
+
+    if request.method=='POST':
+        WDMFO=WebpageModelForm(request.POST)
+        if WDMFO.is_valid():
+            WDMFO.save()
+            return HttpResponse('Webpage  is created')
+        else:
+            return HttpResponse('Invalid Data')
+
+    return render(request,'insert_webpage_by_mf.html',d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
